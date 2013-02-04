2eeb463cc9e8a5f7cd9922390cbc6a288067b3f4
Darren Maczka <dmaczka@vt.edu>
2013-02-03 20:43:56 -0500

    Add usage instructions to number_generator.sh

--- examples/number_generator.sh
+++ examples/number_generator.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ -z "$1" ]; then
+    echo "Dirt simple number generator" >&2
+    echo "Usage: $(basename $0) [integer]" >&2
+    exit 1
+fi
+
 for i in $(seq 1 1 $1)
 do
    echo $i
